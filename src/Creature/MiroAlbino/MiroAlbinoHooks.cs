68ab196f3aa69f0c5bf1ff2dd3107a79d70a358e
BensoneWhite <145090119+BensoneWhite@users.noreply.github.com>
2024-02-12 22:15:19 -0700

    Witness Select Screen

--- src/Creature/MiroAlbino/MiroAlbinoHooks.cs
+++ src/Creature/MiroAlbino/MiroAlbinoHooks.cs
@@ -4,17 +4,18 @@ public class MiroAlbinoHooks
 {
     public static void Apply()
     {
-        On.MirosBirdGraphics.ctor += MirosBirdGraphics_ctor;
+        On.MirosBirdGraphics.Update += MirosBirdGraphics_Update;
         On.MirosBird.Update += MirosBird_Update;
+    }
+
+    private static void MirosBirdGraphics_Update(On.MirosBirdGraphics.orig_Update orig, MirosBirdGraphics self)
+    {
+        orig(self);
+        self.eyeCol = Color.white;
     }
 
     private static void MirosBird_Update(On.MirosBird.orig_Update orig, MirosBird self, bool eu)
     {
         orig(self, eu);
-    }
-
-    private static void MirosBirdGraphics_ctor(On.MirosBirdGraphics.orig_ctor orig, MirosBirdGraphics self, PhysicalObject ow)
-    {
-        orig(self, ow);
     }
 }
